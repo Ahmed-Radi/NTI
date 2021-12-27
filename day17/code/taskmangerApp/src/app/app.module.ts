138a9926fcf6ba7920c3f9ac64dd77c27d803d86
Ahmed Radi <ahmedradi743@gmail.com>
2021-12-27 16:28:23 +0200

    Code - day17

---
+++ day17/code/taskmangerApp/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './views/sign-up/sign-up.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AuthService } from './services/auth.service';
+import { ProfileComponent } from './views/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
